d0d1b4f9db78d763e91bb80e28526054f8a96b4d
Seeteawhy <847362035@qq.com>
2020-12-08 21:31:02 +0800

    first commit

--- lodash/seeteawhy-lodash.js
+++ lodash/seeteawhy-lodash.js
@@ -1,1 +1,14 @@
-//test
+var seeteawhy = function () {
+  function compact(ary) {//创建一个新数组，包含原数组中所有的非假值元素：遍历老数组，push新数组真值
+    let newArray = []
+    for (let i = 0; i < ary.length; i++){
+      if (ary[i]) {
+        newArray.push(ary[i])
+      }
+    }
+    return newArray;
+  }
+  return {
+    compact,
+  }
+}()
