2fbf2dc96a3dbc61056052bd766382c968a716f1
Seeteawhy <847362035@qq.com>
2020-12-09 12:15:06 +0800

    add chunk modifications

--- lodash/seeteawhy-lodash.js
+++ lodash/seeteawhy-lodash.js
@@ -3,7 +3,8 @@ var seeteawhy = function () {
   //将数组（array）拆分成多个 size 长度的区块，
   //并将这些区块组成一个新数组。 如果array 无法被分割成全部等长的区块，
   //那么最后剩余的元素将组成一个区块。
-  //定义l为数组长度，如果剩余长度大于size，循环执行拆分操作，如果小于size，剩余元素为最后一个数组
+
+  //自己思路：定义l为数组长度，如果剩余长度大于size，循环执行拆分操作，如果小于size，剩余元素为最后一个数组
   function chunk(array, size) {
     let l = array.length
     let result = []
@@ -21,6 +22,30 @@ var seeteawhy = function () {
     result.push(array)
     return result
   }
+  return {
+    chunk,
+  }
+  /*官方思路：将原数组截成length/size段，slice进新数组
+  function chunk(array, size) {
+    length = array == null ? 0 : array.length
+    
+    if (!length || size < 1) {
+      return []
+    }
+    let index = 0
+    let resIndex = 0
+    const result = new Array(Math.ceil(length / size))
+    while (index < length) {
+      result[resIndex++] = array.slice(index,(index += size)) //exceed case?
+    }
+    return result
+  }
+  chunk(['a', 'b', 'c', 'd'], 3)
+  return {
+    chunk,
+  }
+  */
+
   
   
   
