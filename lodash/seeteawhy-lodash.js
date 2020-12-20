6b074a2463aab88ddbac3ca4e43045d4f8988a92
Seeteawhy <847362035@qq.com>
2020-12-20 18:54:15 +0800

    update

--- lodash/seeteawhy-lodash.js
+++ lodash/seeteawhy-lodash.js
@@ -2,21 +2,7 @@ var seeteawhy = function () {
   //-----------------------数组-------------------------
 
 
-  //将原数组截成length/size段，slice进新数组
-  function chunk(array, size) {
-    const length = array == null ? 0 : array.length
-    
-    if (!length || size < 1) {
-      return []
-    }
-    let index = 0
-    let resIndex = 0
-    const result = new Array(Math.ceil(length / size))
-    while (index < length) {
-      result[resIndex++] = array.slice(index,(index += size)) 
-    }
-    return result
-  }
+
 
   //创建一个新数组，包含原数组中所有的非假值元素：遍历老数组，push新数组真值
   function compact(ary) {
@@ -192,7 +178,7 @@ var seeteawhy = function () {
 
   function nth(array, n = 0) {
     if (n < 0) {
-      n = array.length - 1
+      n = array.length + n
     }
     return array[n]
   }
@@ -263,6 +249,183 @@ var seeteawhy = function () {
     return result
   }
 
+  //获取对象路径处的值, 如果解析未定义, 就返回默认值
+  function get(object, path, defaultValue = 'undefined') {
+    let names = path.split('.')
+    for (let name of names) {
+      if (name in Object(object)) {
+        object = object[name]
+      } else {
+        return defaultValue
+      }
+    }
+    return object
+  }
+
+  //创建一个切片数组，去除array中从 predicate 返回假值开始到尾部的部分。
+  function dropRightWhile(ary, predicate) {
+    
+    var result = ary
+    var f = iteratee(predicate)
+    for (var i = result.length - 1; i >= 0; i--) {
+        if (!f(result[i])) {
+            break
+        } 
+    }
+    return result.slice(0,i+1)
+  }
+
+  //创建一个切片数组，去除array中从起点开始到 predicate 返回假值结束部分。
+  function dropWhile(ary, predicate) {
+    let result = ary
+    predicate = iteratee(predicate)
+    for (var i = 0; i < ary.length; i++){
+      if (!predicate(result[i])) {
+        break
+      }
+    }
+    return result.slice(i, ary.length)
+  }
+
+  //返回第一个通过 predicate 判断为真值的元素的索引值
+  function findIndex(array, predicate, fromIndex = 0) {
+    let fidx = iteratee(predicate)
+    for (var i = fromIndex; i < array.length; i++){
+      if (fidx(array[i])) {
+        break
+      }
+    }
+    return i
+  }
+
+  function findLastIndex(array, predicate, fromIndex = array.length - 1) {
+    let flidx = iteratee(predicate)
+    for (var i = fromIndex; i >= 0; i--){
+      if (flidx(array[i])) {
+        break
+      }
+    }
+    return i
+  }
+
+  //选出最短的数组，和其他数组比
+  function intersection(...arrays) {
+    let result = []
+    let comparison = arrays.shift()
+    var flag = 1
+    comparison.filter(function (item, index) {
+      for (var arr of arrays) {
+        if (!arr.includes(item)) {
+          flag = -1
+        }
+      }
+      if (flag === 1) {
+        result.push(item)
+      }
+      
+      })
+    return result
+  }
+  
+
+
+
+  //---------------------------self use -----------------------
+  //创建一个返回给定对象的 path 的值的函数。
+  //path (Array|string): 要得到值的属性路径。
+  function property(path) {
+    return function (obj) {
+      var value = Object.assign({},obj)
+      if (typeof path === 'string') {
+        let ary = path.split('.')
+        for (let i of ary) {
+          value = value[i]
+        }
+      } else {
+        for (let i of path) {
+          value = value[i]
+        }
+      }
+      return value
+    }
+  }
+
+  function isMatch(obj,src) {
+    return function (obj) {
+      for (let key in src) {
+        if (src[key] && typeof src[key] === 'object') {
+          if (!isMatch(obj[key], src[key])) {
+            return false
+          }
+        } else {
+          if (src[key] !== obj[key]) {
+            return false
+          }
+        }
+        
+      }
+      return true
+    }
+  }
+
+  function matches(source) {
+    return function (obj) {
+      for (let key of Object.keys(source)) {
+        if (source[key] != obj[key]) {
+          return false
+        }
+      }
+      return true
+    }
+  }
+  /**
+   * 创建一个函数，判断给定对象属性是否与传入的参数相同，相同返回true，不同返回false
+   * @param {Array|string} path 待获取的属性名
+   * @param {*} srcValue 待比较的属性值
+   * @returns {Function}
+   */
+  function matchesProperty(ary) {//讲了啥哦
+    return this.matches(this.fromPairs(this.chunk(ary, 2)));
+
+  }
+
+  function iteratee(predicate) {
+    if (typeof predicate === 'function') {
+      return predicate
+    }
+    if (typeof predicate === 'string') {
+      return property(predicate)
+    }
+    if (Array.isArray(predicate)) {
+      return matchesProperty(predicate)
+    }
+    if (predicate instanceof Object) {
+      return matches(predicate)
+    }
+  }
+  //将原数组截成length/size段，slice进新数组
+  function chunk(array, size) {
+    const length = array == null ? 0 : array.length
+    
+    if (!length || size < 1) {
+      return []
+    }
+    let index = 0
+    let resIndex = 0
+    const result = new Array(Math.ceil(length / size))
+    while (index < length) {
+      result[resIndex++] = array.slice(index,(index += size)) 
+    }
+    return result
+  }
+  //遍历数组，把每个键值对加到新建到对象，返回对象
+  function fromPairs(array) {
+    let newobj = {}
+    for (let i = 0; i < array.length; i++){
+      newobj[array[i][0]] = array[i][1]
+    }
+    return newobj
+  }
 
 
 
@@ -333,14 +496,20 @@ var seeteawhy = function () {
     uniq,
     identity,
     concat,
+    property,
     max,
     min,
     sum,
-    property(str) {
-      return function (obj) {
-        return obj[str];
-      };
-    },
+    isMatch,
+    matches,
+    matchesProperty,
+    iteratee,
+    get,
+    dropRightWhile,
+    dropWhile,
+    findIndex,
+    findLastIndex,
+    intersection
 
   }
 }()
